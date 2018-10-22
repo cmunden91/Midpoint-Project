f201261f2e2792d90628b1ab2296561542b3d34d
Christopher Munden <cmunden@alumni.unca.edu>
2018-10-22 18:27:48 -0400

    Started work on recaptcha.

--- scripts/main.js
+++ scripts/main.js
@@ -4,6 +4,6 @@ const app = express()
 const port = 3000
 var path = require("path");
 //Routing for Home Page
-app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/index.html')))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname+'/../index.html')))
 //Init Server and Run Code
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
